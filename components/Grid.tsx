f60a0ca976379d2b28fc81b6fd0f0d6f9eeaa58c
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2024-07-03 12:00:35 +0200

    commit before deploying

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,7 +4,7 @@ import { gridItems } from "@/data";
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid className="w-full py-20">
+      <BentoGrid className="w-full ">
         {gridItems.map(
           ({
             id,
